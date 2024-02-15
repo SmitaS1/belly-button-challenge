cd27e308d7bffec3d87ba3555d717a9dd400c55f
SmitaS1 <smitashah0405@gmail.com>
2024-02-15 15:26:52 -0500

    challenge 14 files are updated here

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -110,66 +110,96 @@ function metadatainfo(id_value){
 init();
 
 // Bonus Gauge chart
-function panelInfo(id) {
-  d3.json(url).then(function (data) {
-      let sampleData = data;
-      let metadata = sampleData.metadata;
-      let identifier = metadata.filter(sample =>
-          sample.id.toString() === id)[0];
-      let panel = d3.select('#sample-metadata');
-      panel.html('');
-      Object.entries(identifier).forEach(([key, value]) => {
-          panel.append('h6').text(`${key}: ${value}`);
-      })
-      let gaugeTrace = {
-          domain: { x: [0, 5], y: [0, 1] },
-          type: "indicator",
-          ids: ['0-1', '1-2', '2-3', '3-4', '5-6', '6-7', '7-8', '8-9'],
-          gauge: {
-              axis: {
-                  range: [0, 9],
-                  tickmode: 'linear'
-              },
-              steps: [
-                  { range: [0, 1], color: 'rgb(248,243,236)', id: '0-1' },
-                  { range: [1, 2], color: 'rgb(244,241,228)', name: '1-2' },
-                  { range: [2, 3], color: 'rgb(233,230,201)', name: '2-3' },
-                  { range: [3, 4], color: 'rgb(229,232,176)', name: '3-4' },
-                  { range: [4, 5], color: 'rgb(213,229,153)', name: '4-5' },
-                  { range: [5, 6], color: 'rgb(183,205,143)', name: '5-6' },
-                  { range: [6, 7], color: 'rgb(138,192,134)', name: '6-7' },
-                  { range: [7, 8], color: 'rgb(137,188,141)', name: '7-8' },
-                  { range: [8, 9], color: 'rgb(132,181,137)', name: '8-9' },
-              ]
-          },
-          mode: "gauge"
-      };
-      let deg = (180 / 9) * identifier.wfreq;
-      let radius = 0.5;
-      let radians = (deg * Math.PI) / 180;
-      let x = -1 * radius * Math.cos(radians);
-      let y = radius * Math.sin(radians);
-      let guageLayout = {
-          title: "<b>Belly Button Washing Frequency</b> <br>Scrubs Per Week</br>",
-          shapes: [{
-              type: 'line',
-              x0: 0.5,
-              y0: 0,
-              x1: x + 0.5,
-              y1: y + 0.5,
-              line: {
-                  color: 'red',
-                  width: 4
-              }
-          }],
-          xaxis: { visible: true, range: [-1, 1] },
-          yaxis: { visible: true, range: [-1, 1]},
-          width: 800,
-          height: 500
 
-      };
-     
-      let gaugeData = [gaugeTrace];
-      Plotly.newPlot('gauge', gaugeData, guageLayout);
-  })
+function panelInfo(id_value) {
+  // Enter the washing frequency between 0 and 180
+  let level = parseFloat(id_value) * 20;
+
+  // Trig to calc meter point
+  let degrees = 180 - level;
+  let radius = 0.5;
+  let radians = (degrees * Math.PI) / 180;
+  let x = radius * Math.cos(radians);
+  let y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+  let pathX = String(x);
+  let space = " ";
+  let pathY = String(y);
+  let pathEnd = " Z";
+  let path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+  let data = [
+    {
+      type: "scatter",
+      x: [0],
+      y: [0],
+      marker: { size: 12, color: "850000" },
+      showlegend: false,
+      name: "Freq",
+      text: level,
+      hoverinfo: "text+name"
+    },
+    {
+      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+      rotation: 90,
+      text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      textinfo: "text",
+      textposition: "inside",
+      marker: {
+        colors: [
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
+        ]
+      },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+    }
+  ];
+
+  let layout = {
+    shapes: [
+      {
+        type: "path",
+        path: path,
+        fillcolor: "850000",
+        line: {
+          color: "850000"
+        }
+      }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 500,
+    width: 500,
+    xaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    },
+    yaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    }
+  };
+
+  let GAUGE = document.getElementById("gauge");
+  Plotly.newPlot(GAUGE, data, layout);
 }
+
+
+
